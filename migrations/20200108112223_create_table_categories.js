254ce6b744dc272dd9fe07c9ef7d92bbf73f6536
BrunnaMaiaradaSilva <silva.brunnam@gmail.com>
2020-01-08 14:05:59 -0200

    Create Database and migrations

--- migrations/20200108112223_create_table_categories.js
+++ migrations/20200108112223_create_table_categories.js
@@ -1,8 +1,19 @@
 
 exports.up = function(knex, Promise) {
-  
+
+    return knex.schema.createTable('categories', tabele => {
+
+        table.increments('id').primary()
+        table.string('name').notNull()
+        table.integer('parentId').references('id')
+            .inTable('categories')
+
+    })
+
 };
 
 exports.down = function(knex, Promise) {
+
+    return knex.schema.dropTable('categories')
   
 };
